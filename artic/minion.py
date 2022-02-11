f6a66abce991bdf5dfadd5c429520978dda41d9f
Biowilko <sam1.wilkinson1@gmail.com>
2022-02-11 11:11:13 +0000

    re-implemented scheme downloader in python

--- artic/minion.py
+++ artic/minion.py
@@ -5,6 +5,8 @@ from Bio import SeqIO
 import os
 import sys
 import time
+import requests
+import hashlib
 from .vcftagprimersites import read_bed_file
 
 def get_nanopolish_header(ref):
@@ -26,13 +28,24 @@ def get_nanopolish_header(ref):
         raise SystemExit(1)
     return  "%s:%d-%d" % (recs[0].id, 1, len(recs[0])+1)
 
+def check_scheme_hashes(filepath, manifest_hash):
+    with open(filepath, "rb") as fh:
+        data = fh.read()
+        hash_sha256 = hashlib.sha256(data).hexdigest()
+    if hash_sha256 != manifest_hash:
+        print(
+            colored.yellow(
+                f"sha256 hash for {str(filepath)} does not match manifest"
+            ),
+            file=sys.stderr,
+        )
+        raise SystemExit(1)
+
 def get_scheme(scheme_name, scheme_directory, scheme_version="1"):
     """Get and check the ARTIC primer scheme.
-
     When determining a version, the original behaviour (parsing the scheme_name and
     separating on /V ) is used over a specified scheme_version. If neither are
     provided, the version defaults to 1.
-
     If 0 is provided as version, the latest scheme will be downloaded.
  
     Parameters
@@ -43,7 +56,6 @@ def get_scheme(scheme_name, scheme_directory, scheme_version="1"):
         The directory containing the primer scheme and reference sequence
     scheme_version : str
         The primer scheme version (optional)
-
     Returns
     -------
     str
@@ -54,39 +66,84 @@ def get_scheme(scheme_name, scheme_directory, scheme_version="1"):
         The version being used
     """
     # try getting the version from the scheme name (old behaviour)
-    if scheme_name.find('/V') != -1:
-        scheme_name, scheme_version = scheme_name.split('/V')
-    
+    if scheme_name.find("/V") != -1:
+        scheme_name, scheme_version = scheme_name.split("/V")
+
     # create the filenames and check they exist
-    bed = "%s/%s/V%s/%s.scheme.bed" % (scheme_directory, scheme_name, scheme_version, scheme_name)
-    ref = "%s/%s/V%s/%s.reference.fasta" % (scheme_directory, scheme_name, scheme_version, scheme_name)
+    bed = "%s/%s/V%s/%s.scheme.bed" % (
+        scheme_directory,
+        scheme_name,
+        scheme_version,
+        scheme_name,
+    )
+    ref = "%s/%s/V%s/%s.reference.fasta" % (
+        scheme_directory,
+        scheme_name,
+        scheme_version,
+        scheme_name,
+    )
     if os.path.exists(bed) and os.path.exists(ref):
         return bed, ref, scheme_version
 
     # if they don't exist, try downloading them to the current directory
-    print(colored.yellow("could not find primer scheme and reference sequence, attempting to download"), file=sys.stderr)
-    getScheme = "artic-tools get_scheme %s --schemeVersion %s" % (scheme_name, scheme_version)
-    print(colored.green("Running: "), getScheme, file=sys.stderr)
-    if (os.system(getScheme) != 0):
-        print(colored.red("scheme download failed"), file=sys.stderr)
-        raise SystemExit(1)
-    downloadedBed = "%s.v%s.primer.bed" % (scheme_name, scheme_version)
-    downloadedRef = "%s.v%s.reference.fasta" % (scheme_name, scheme_version)
-    if not (os.path.exists(downloadedBed) and os.path.exists(downloadedRef)):
-        print(colored.red("could not find/download primer scheme: ") + scheme_name)
-        raise SystemExit(1)
+    print(
+        colored.yellow(
+            "could not find primer scheme and reference sequence, downloading"
+        ),
+        file=sys.stderr,
+    )
 
-    # now put them where they pipeline usually expects them to save us downloading them on future runs
-    targetDir = os.path.dirname(bed)
-    if not os.path.exists(targetDir):
-        try:
-            os.makedirs(targetDir)
-        except OSError as error:
-            print(colored.red("could not create provided scheme directory ({}): {}") .format(targetDir, error))
-            raise SystemExit(1)
-    os.rename(downloadedBed, bed)
-    os.rename(downloadedRef, ref)
-    return bed, ref, scheme_version
+    with requests.get(
+        "https://raw.githubusercontent.com/artic-network/primer-schemes/master/schemes_manifest.json"
+    ) as fh:
+        manifest = fh.json()
+    
+    for scheme, scheme_contents in dict(manifest["schemes"]).items():
+        if scheme == scheme_name.lower() or scheme_name.lower() in scheme_contents["aliases"]:
+            print(
+                colored.yellow(
+                    f"\tfound requested scheme:\t{scheme} (using alias {scheme_name})"
+                ),
+                file=sys.stderr,
+            )
+            if scheme_version == 0:
+                print(
+                    colored.yellow(
+                        f"Latest version for scheme {scheme} is -> {scheme_contents['latest_version']}"
+                    ),
+                    file=sys.stderr,
+                )
+                scheme_version = scheme_contents["latest_version"]
+            elif scheme_version not in dict(scheme_contents["primer_urls"]).keys():
+                print(
+                    colored.yellow(
+                        f"Requested scheme version {scheme_version} not found; using latest version ({scheme_contents['latest_version']}) instead"
+                    ),
+                    file=sys.stderr,
+                )
+                scheme_version = scheme_contents["latest_version"]
+            
+
+            os.makedirs(os.path.dirname(bed), exist_ok=True)
+            with requests.get(scheme_contents["primer_urls"][scheme_version]) as fh:
+                open(bed, 'wb').write(fh.content)
+            
+            os.makedirs(os.path.dirname(ref), exist_ok=True)
+            with requests.get(scheme_contents["reference_urls"][scheme_version]) as fh:
+                open(ref, 'wb').write(fh.content)
+            
+            check_scheme_hashes(bed, scheme_contents["primer_sha256_checksums"][scheme_version])
+            check_scheme_hashes(ref, scheme_contents["reference_sha256_checksums"][scheme_version])
+
+            return bed, ref, scheme_version
+
+    print(
+        colored.yellow(
+            f"\tRequested scheme:\t{scheme_name} could not be found, exiting"
+        ),
+        file=sys.stderr,
+    )
+    raise SystemExit(1)
 
 def run(parser, args):
 
