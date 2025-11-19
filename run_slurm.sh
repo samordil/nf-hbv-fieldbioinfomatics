d717626ea767f1a48359f2e66ce4446bb311df33
Samuel Odoyo <samordil@gmail.com>
2025-11-19 09:26:39 +0300

    Update pipeline

--- run_slurm.sh
+++ run_slurm.sh
@@ -8,15 +8,15 @@
 #SBATCH -o job.%j.out
 #SBATCH -e job.%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=sodoyo@kemri-wellcome.org
+#SBATCH --mail-user=use_your_email@gmail.com    # Change to your email
 
-export NXF_WORK=/scratch/hbv_nxf_work
-export TMPDIR=/scratch/hbv_tmp
+export NXF_WORK=/scratch/hbv_nxf_work      # Change to your instituion scratch dir
+export TMPDIR=/scratch/hbv_tmp             # Change to your instituion scratch dir
 mkdir -p "$NXF_WORK" "$TMPDIR"
 
 # Activate existing conda environment
-source ~/miniforge3/etc/profile.d/conda.sh
-conda activate hbv-artic
+source ~/miniforge3/etc/profile.d/conda.sh   # Change to point ot you custom environment
+conda activate hbv-artic    # change to activate the custom environment
 
 # Run the pipeline
 nextflow run main.nf -profile hbv,slurm -resume
